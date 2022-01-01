74b8e128612fb1da2fe3972412f782253a694f10
Abdullah <A21alharbi9@gmail.com>
2022-01-01 21:11:39 +0300

    Added validation

--- signupForm.php
+++ signupForm.php
@@ -22,36 +22,107 @@
 
         if(isset($_POST['signupBtn'])){
 
-            $userFirstName= $_POST['firstName'];
-            $userLastName= $_POST['lastName'];
-            $userEmail= $_POST['email'];
-            $userPassword= $_POST['password'];
-
-            $passwordHash= password_hash($userPassword,PASSWORD_ARGON2I);  
-        
-            $alreadyExists= $pdo->prepare("SELECT * FROM users_data WHERE email=:email");
-            $sql= $pdo->prepare("INSERT INTO users_data (firstName, lastName, email, password) VALUES (:firstName,:lastName,:email,:password)");
-
-            $alreadyExistsSqlValues=['email'=>$userEmail];
-            $sqlValues=['firstName'=>$userFirstName,'lastName'=>$userLastName,'email'=>$userEmail,'password'=>$passwordHash];
             
-            $alreadyExists->execute($alreadyExistsSqlValues);
-            $sql->execute($sqlValues);
+            
+            $userFirstName= trim($_POST['firstName']);
+            $userLastName= trim($_POST['lastName']);
+            $userEmail= trim($_POST['email']);
+            $userPassword= trim($_POST['password']);
+            $userConfirmPassword= trim($_POST['confirmPassword']);
 
-            if($alreadyExists->rowCount()>0){
-                echo 'User already exists!';
+            if(empty($userFirstName)){
+
+                $firstNameError = 'First name is required.';
+
             }else{
-                
-                if($sql->rowCount()>0){
 
-                    echo 'Success';
+                if(!preg_match('/^[A-Za-z]+$/', $userFirstName)){
                     
-                }else{
-
-                    echo 'Fail';
+                    $firstNameError = 'Invalid first name.';
+                    
                 }
             }
-        }
+            if(empty($userLastName)){
+
+                $lastNameError = 'Last name is required.';
+
+            }else{
+
+                if(!preg_match('/^[A-Za-z]+$/', $userLastName)){
+                    
+                    $lastNameError = 'Invalid last name.';
+                    
+                }
+            }
+            if(empty($userEmail)){
+                
+                 $emailError = 'Email is required.';
+
+            }else{
+
+                if(!preg_match('/([A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]+)/', $userEmail)){
+                    
+                    $emailError = 'Invalid email.';
+                    
+                }
+            }
+            if(empty($userPassword)){
+                
+                $passwordError = 'Password is required.';
+                
+            }else{
+
+                if(!preg_match('/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{8,})\S$/', $userPassword)){
+                    
+                    $passwordError = 'Password must contain at least 1 captial letter, 1 small letter and 1 number.';
+                    
+                }
+                
+            }
+            if(empty($userConfirmPassword)){
+
+                $confirmPasswordError = 'confirm password is required.';
+                
+            } else{
+
+                if($userConfirmPassword !== $userPassword){
+                    
+                    $confirmPasswordError = 'Passwords should match.';
+                    
+                }
+            }    
+                
+            $errors = !isset($firstNameError) && !isset($lastNameError) && !isset($emailError) && !isset($passwordError)&& !isset($confirmPasswordError);
+            
+            if(!isset($firstNameError) && !isset($lastNameError) && !isset($emailError) && !isset($passwordError)&& !isset($confirmPasswordError)){
+
+                $passwordHash= password_hash($userPassword,PASSWORD_ARGON2I);  
+                
+                $alreadyExists= $pdo->prepare("SELECT * FROM users_data WHERE email=:email");
+                $sql= $pdo->prepare("INSERT INTO users_data (firstName, lastName, email, password) VALUES (:firstName,:lastName,:email,:password)");
+                
+                $alreadyExistsSqlValues=['email'=>$userEmail];
+                $sqlValues=['firstName'=>$userFirstName,'lastName'=>$userLastName,'email'=>$userEmail,'password'=>$passwordHash];
+                
+                $alreadyExists->execute($alreadyExistsSqlValues);
+                $sql->execute($sqlValues);
+                
+                if($alreadyExists->rowCount()>0){
+                    $dbMessageResult = 'User already exists.';
+                }else{
+                    
+                    if($sql->rowCount()>0){
+                        
+                        $dbMessageResult = 'Success';
+                        
+                    }else{
+                        
+                        $dbMessageResult = 'An error occured';
+
+                        }
+                    }
+                }
+            }
     } catch (PDOException $e) {
 
             echo 'Error: '. $e->getMessage();
@@ -64,14 +135,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
    <link href="./styles.css" rel="stylesheet"/>
-    <title>PHP practice</title>
+    <title>PHP signup/login</title>
 </head>
 <body>
     <?php
-    echo "<h1>Hello</h1>","<h1>World</h1>";
+    echo "<h1 class='my-3' >Hello World</h1>";
     ?>
     
     <div class="d-flex flex-column justify-content-center align-items-center" id="signupContainer">
@@ -79,24 +149,83 @@
             <h5>Sign up</h5>
         </div>
         <form action="signupForm.php" method="post" class="d-flex flex-column" id="formContainer">
-            <div class="">
-                <label class="form-label">
+           
+                <?php if(isset($dbMessageResult)):
+                    echo "<span class=' text-center text-danger font-weight-bold'>$dbMessageResult</span>";
+                    endif
+                ?>
+            
+            <div class="d-flex flex-column justify-content-center align-items-start m
+            t-2">
+
+                <label class="firstName mb-2" for="firstName">
                     First name:
-            </label>
-            <input type="text" name="firstName" class="form-control"/>
-        </div>
-        <label class="form-label">
-            Last name:
-        </label>
-        <input type="text" name="lastName" class="form-control"/>
-        <label class="form-label">
-            Email:
-        </label>
-        <input type="email" name="email" class="form-control"/>
-        <label class="form-label">
-            Password:
-        </label>
-        <input type="password" name="password" class="form-control"/>
+                </label>
+                <input type="text" name="firstName" class="form-control" value="<?php if($errors === false) { echo $userFirstName;} ?>"/> 
+
+            </div>
+           
+                <?php if(isset($firstNameError)):
+                    echo "<span class='text-danger font-weight-bold'>$firstNameError</span>";
+                    endif
+                ?>
+
+            <div class="d-flex flex-column justify-content-start align-items-start">
+
+            <label class="lastName mb-2" for="lastName">
+                    Last name:
+                </label>
+                <input type="text" name="lastName" class="form-control" value="<?php if($errors === false) { echo $userLastName;} ?>"/>
+                
+            </div>
+
+                <?php if(isset($lastNameError)):
+                    echo "<span class='text-danger font-weight-bold'>$lastNameError</span>";
+                    endif
+                ?>
+
+            <div class="d-flex flex-column justify-content-center align-items-start">
+
+                <label class="form-label" for="email">
+                    Email:
+                </label>
+                <input type="email" name="email" class="form-control" value="<?php if($errors === false) { echo $userEmail;} ?>"/>
+
+            </div>
+           
+                <?php if(isset($emailError)):
+                    echo "<span class='text-danger font-weight-bold'>$emailError</span>";
+                    endif
+                ?>
+
+            <div class="d-flex flex-column justify-content-center align-items-start">
+                
+                <label class="form-label" for="password">
+                    Password:
+                </label>
+                <input type="password" name="password" class="form-control" value="<?php if($errors === false) { echo $userPassword;} ?>"/>
+                
+            </div>
+
+                <?php if(isset($passwordError)):
+                    echo "<span class='text-danger font-weight-bold'>$passwordError</span>";
+                    endif
+                    ?>
+
+            <div class="d-flex flex-column justify-content-center align-items-start">
+
+                <label class="form-label" for="confirmPassword">
+                    Confirm password:
+                </label>
+                <input type="password" name="confirmPassword" class="form-control" value="<?php if($errors === false) { echo $userConfirmPassword;} ?>"/>
+
+            </div>
+
+                <?php if(isset($confirmPasswordError)):
+                    echo "<span class='text-danger font-weight-bold'>$confirmPasswordError</span>";
+                    endif
+                ?>
+
         <button type="submit" name="signupBtn">Submit</button>
     </form>
     <div class="d-flex">
